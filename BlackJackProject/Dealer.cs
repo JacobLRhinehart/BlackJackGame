c0f73f2fcf002983347d87493d4cd79578c4d973
Jacob Rhinehart <jacoblrhinehart@gmail.com>
2020-09-17 21:50:59 -0400

    Add project files.

---
+++ BlackJackProject/Dealer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJackProject
+{
+    public class CardValueGenerator
+    {
+        public int Deal()
+        {
+            int sum = RandomCardNumber() + RandomCardNumber();
+            return sum;
+        }
+
+        public int RandomCardNumber()
+        {
+            Random r = new Random();
+            int genRandom = r.Next(1, 11);
+            return genRandom;
+        }
+    }
+}
---
+++ BlackJackProject/ProgramUI.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJackProject
+{
+    class ProgramUI
+    {
+
+        public void Run()
+        {
+            Menu();
+        }
+        // Menu
+
+        private void Menu()
+        {
+            bool keepRunning = true;
+            while (keepRunning)
+            {
+                Console.WriteLine("Welcome to Blackjack!!!\n" +
+                  "1. Play a new game of Blackjack!\n" +
+                  "2. Continue to play!\n" +
+                  "3. Exit the game!");
+
+                //Get users input
+
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        GameMenu();
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        Console.WriteLine("Thank you for playing Blackjack!");
+                        Console.WriteLine("Press any key to continue...");
+                        Console.ReadKey();
+                        keepRunning = false;
+                        break;
+                    default:
+                        Console.WriteLine("Please choose an option, 1-3.");
+                        break;
+                }
+            }
+        }
+
+        public void GameMenu()
+        {
+            bool keepRunning = true;
+            while (keepRunning)
+            {
+                Console.Clear();
+                Console.WriteLine("Here We Go!\n" +
+                    "1. Hit\n" +
+                    "2. Stay\n" +
+                    "3. Exit");
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        Hit();
+                        break;
+                    case "2":
+                        Stay();
+                        break;
+                    case "3":
+                        Console.WriteLine("Thank you for playing Blackjack!");
+                        Console.ReadKey();
+                        keepRunning = false;
+                        break;
+                    default:
+                        Console.WriteLine("Please choose an option, 1-3.");
+                        break;
+                }
+            }
+
+
+        }
+
+        public void Hit()
+        {
+            CardValueGenerator cardValueGenerator = new CardValueGenerator();
+            int cardValueGenerator.Deal
+
+
+        }
+
+        public void Stay()
+        {
+
+        }
+
+
+
+    }
+}
+
+
